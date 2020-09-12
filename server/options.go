0356a8069a344f6c7e5c3026e36d37f7b3baff1f
Jonathan Fenwick <jonathan.fenwick@delineate.io>
2020-09-12 13:36:14 +0100

    Refactor the Options functions

--- server/options.go
+++ server/options.go
@@ -21,12 +21,18 @@ func NewDefaultOptions() (*Options, error) {
 		Env:      vars.Read("DIO_ENV", "io"),
 		Location: vars.Read("DIO_LOCATION", "/config"),
 		Routes:   []routes.Route{},
+		Entities: make([]interface{}, 0),
 	}, nil
 }
 
-// Add is used to add another route to the
-func (o *Options) Add(method, path string, handler handlers.Handler) {
+// AddRoute is used to add another route to the
+func (o *Options) AddRoute(method, path string, handler handlers.Handler) {
 	o.Routes = append(o.Routes, routes.Route{
 		Method: method, Path: path, Handler: handler,
 	})
+}
+
+// AddEntities is used to add the required entities for migration
+func (o *Options) AddEntities(entities ...interface{}) {
+	o.Entities = append(o.Entities, entities...)
 }
